5214a8a37a117c94fefd501c67590b150c596d95
Stefanocab <95408691+Stefanocab@users.noreply.github.com>
2022-05-18 10:41:07 +0200

    Add files via upload

---
+++ Gioco_drag&drop/script.js
@@ -0,0 +1,415 @@
+
+var quiz = new Array();
+
+for(i = 0; i < 34; i++){
+    quiz[i] = new Array();
+}
+
+
+quiz[0][0] = "Quanti fusi orari ci sono in Russia?";
+quiz[0][1] = "11";
+quiz[0][2] = "10";
+quiz[0][3] = "7";
+quiz[0][4] = "13";
+
+quiz[1][0] = "Qual è il fiore nazionale del Giappone?";
+quiz[1][1] = "Fiore di ciliegio";
+quiz[1][2] = "Fiore di sambuca";
+quiz[1][3] = "Fiore di loto";
+quiz[1][4] = "Fiore di lime";
+
+quiz[2][0] = "Quante strisce ci sono sulla bandiera americana?";
+quiz[2][1] = "13";
+quiz[2][2] = "12";
+quiz[2][3] = "11";
+quiz[2][4] = "14";
+
+quiz[3][0] = "Qual è l’animale nazionale dell’Australia?";
+quiz[3][1] = "Canguro rosso";
+quiz[3][2] = "Tasso";
+quiz[3][3] = "Koala";
+quiz[3][4] = "Dingo";
+
+quiz[4][0] = "Quanti settimane ci vogliono affinché la Terra orbiti attorno al Sole?";
+quiz[4][1] = "52";
+quiz[4][2] = "54";
+quiz[4][3] = "50";
+quiz[4][4] = "47";
+
+quiz[5][0] = "Quale dei seguenti imperi non aveva una lingua scritta?";
+quiz[5][1] = "Inca";
+quiz[5][2] = "Azteco";
+quiz[5][3] = "Egiziano";
+quiz[5][4] = "Romano";
+
+quiz[6][0] = "Fino al 1923, come si chiamava la città turca di Istanbul?";
+quiz[6][1] = "Costantinopoli";
+quiz[6][2] = "Marmara";
+quiz[6][3] = "Islam";
+quiz[6][4] = "Grand Bazaar";
+
+quiz[7][0] = "Quale paese ha il maggior numero di isole al mondo?";
+quiz[7][1] = "Svezia";
+quiz[7][2] = "Grecia";
+quiz[7][3] = "Islanda";
+quiz[7][4] = "Finlandia";
+
+quiz[8][0] = "Qual è il paese più piccolo del mondo?";
+quiz[8][1] = "Il Vaticano";
+quiz[8][2] = "San marino";
+quiz[8][3] = "Monaco";
+quiz[8][4] = "Tuvalu";
+
+quiz[9][0] = "Qual è la capitale del Canada?";
+quiz[9][1] = "Ottawa";
+quiz[9][2] = "Montrèal";
+quiz[9][3] = "Toronto";
+quiz[9][4] = "Victoria";
+
+quiz[10][0] = "Dov’è il posto naturale più basso del pianeta Terra?";
+quiz[10][1] = "Fossa delle Marianne";
+quiz[10][2] = "Napali Coast, Kauai - Hawaii";
+quiz[10][3] = "Lake Louise, Canada";
+quiz[10][4] = "Cappadocia, Turchia";
+
+quiz[11][0] = "Il fiume più lungo del mondo?";
+quiz[11][1] = "Il Nilo";
+quiz[11][2] = "Rio delle ammazoni";
+quiz[11][3] = "Paranà";
+quiz[11][4] = "Lena";
+
+quiz[12][0] = "Da dove viene Billie Eilish?";
+quiz[12][1] = "Los Angeles";
+quiz[12][2] = "Canada";
+quiz[12][3] = "Miami";
+quiz[12][4] = "Florida";
+
+quiz[13][0] = "Da quale città provengono i Beatles?";
+quiz[13][1] = "Liverpool";
+quiz[13][2] = "Manchester";
+quiz[13][3] = "Londra";
+quiz[13][4] = "Brighton";
+
+quiz[14][0] = "Qual è il brano più trasmesso in assoluto su Spotify fino ad oggi?";
+quiz[14][1] = "Ed Sheeran, The shape of you";
+quiz[14][2] = "Justin bieber, Stay";
+quiz[14][3] = "Luis Fonsi, Despacito";
+quiz[14][4] = "Michael Jackson – Thriller";
+
+quiz[15][0] = "Chi è stato il vincitore di sanremo nel 2013?";
+quiz[15][1] = "Marco Mengoni";
+quiz[15][2] = "Emma Marrone";
+quiz[15][3] = "Laura Pausini";
+quiz[15][4] = "Vasco Rossi";
+
+quiz[16][0] = "Quanti tasti ha un pianoforte classico?";
+quiz[16][1] = "88";
+quiz[16][2] = "86";
+quiz[16][3] = "90";
+quiz[16][4] = "83";
+
+quiz[17][0] = "Quale famosa band pop americana era originariamente chiamata “Kara’s Flowers”?";
+quiz[17][1] = "Maroon 5";
+quiz[17][2] = "Coldplay";
+quiz[17][3] = "One Republic";
+quiz[17][4] = "Twenty One Pilots";
+
+quiz[18][0] = "Quale squadra di calcio è conosciuta come “The Red Devils”?";
+quiz[18][1] = "Manchester United";
+quiz[18][2] = "Bayern Monaco";
+quiz[18][3] = "Liverpool";
+quiz[18][4] = "Bari";
+
+quiz[19][0] = "Quale pilota ha vinto il maggior numero di gran premi?";
+quiz[19][1] = "Lewis Hamilton";
+quiz[19][2] = "Michael Schumacher";
+quiz[19][3] = "Ayrton Senna";
+quiz[19][4] = "Niki Lauda";
+
+quiz[20][0] = "Chi è 3a nella classifica di sempre delle campionesse di tennis femminile del Grande Slam?";
+quiz[20][1] = "Serena Williams";
+quiz[20][2] = "Simona Hein";
+quiz[20][3] = "Monica Seles";
+quiz[20][4] = "Sofia Kenin";
+
+quiz[21][0] = "Quando è stata fondata Netflix?";
+quiz[21][1] = "1997";
+quiz[21][2] = "2001";
+quiz[21][3] = "2009";
+quiz[21][4] = "2015";
+
+quiz[22][0] = "Qual è stata la serie più vista su Netflix nel 2019?";
+quiz[22][1] = "Stranger Things";
+quiz[22][2] = "Orange is the new black";
+quiz[22][3] = "Breaking bad";
+quiz[22][4] = "Spartacus";
+
+quiz[23][0] = "Nomina il primo lungometraggio della Pixar?";
+quiz[23][1] = "Toy Story";
+quiz[23][2] = "Monsters & Co";
+quiz[23][3] = "Cars";
+quiz[23][4] = "Nemo";
+
+quiz[24][0] = "Qual è il nome della caffetteria nella sitcom Friends?";
+quiz[24][1] = "Central Perk";
+quiz[24][2] = "Starbucks";
+quiz[24][3] = "Hard rock";
+quiz[24][4] = "No Vacancy";
+
+quiz[25][0] = "Nome del primo film della Disney?";
+quiz[25][1] = "Biancaneve";
+quiz[25][2] = "Pinocchio";
+quiz[25][3] = "Cenerentola";
+quiz[25][4] = "Re leone";
+
+quiz[26][0] = "Dove si trova la sala degli specchi?";
+quiz[26][1] = "Versailles";
+quiz[26][2] = "Parigi";
+quiz[26][3] = "Milano";
+quiz[26][4] = "Tokyo";
+
+quiz[27][0] = "Quale band ha scritto la canzone \"Talking to myself\"?";
+quiz[27][1] = "Linkin park";
+quiz[27][2] = "One direction";
+quiz[27][3] = "One republic";
+quiz[27][4] = "Nirvana";
+
+quiz[28][0] = "Dove si trova il più alto edificio europeo?";
+quiz[28][1] = "Mosca";
+quiz[28][2] = "Torino";
+quiz[28][3] = "Londra";
+quiz[28][4] = "Berlino";
+
+quiz[29][0] = "In che anno si sciolsero i Beatles?";
+quiz[29][1] = "1970";
+quiz[29][2] = "1968";
+quiz[29][3] = "1976";
+quiz[29][4] = "1964";
+
+quiz[30][0] = "Qual è il diametro di un canestro da basket in pollici?";
+quiz[30][1] = "18 pollici";
+quiz[30][2] = "17 pollici";
+quiz[30][3] = "16 pollici";
+quiz[30][4] = "14 pollici";
+
+quiz[31][0] = "Nel basket professionistico, quanto è alto da terra il canestro da basket?";
+quiz[31][1] = "10 piedi";
+quiz[31][2] = "7 piedi";
+quiz[31][3] = "8 piedi";
+quiz[31][4] = "11 piedi";
+
+quiz[32][0] = "Quanto è grande una palla da baseball?";
+quiz[32][1] = "9 pollici di circonferenza";
+quiz[32][2] = "10 pollici di circonferenza";
+quiz[32][3] = "11 pollici di circonferenza";
+quiz[32][4] = "8 pollici di circonferenza";
+
+quiz[33][0] = "Le Olimpiadi si svolgono ogni quanti anni?";
+quiz[33][1] = "4 anni";
+quiz[33][2] = "5 anni";
+quiz[33][3] = "3 anni";
+quiz[33][4] = "2 anni";
+
+
+var corrette=0;
+var temp=300;
+scaduto=false;
+comp= false;
+
+function inizia() {
+    $("#start").hide();
+    const interval = setInterval(timer,1000);
+    timeout = setTimeout(temposcaduto,temp*1000 + 1000);
+    for(i = 0; i < 34; i++) {
+        container = document.createElement("div");
+        container.setAttribute("style", "text-align: center; display: flex; align-items: center; background-color: burlywood;");
+        
+
+        domanda = document.createElement("p");
+        domanda.setAttribute("id", "d" + i);
+        domanda.append("Domanda " + (i+1) + ": " + quiz[i][0]);
+        domanda.setAttribute("style", "text-align: center;");
+        container.append(domanda);
+
+        box = document.createElement("div");
+        box.setAttribute("id", "box");
+        box.setAttribute("style", "text-align: center; background-color: white");
+        box.setAttribute("ondrop", "drop(event)");
+        box.setAttribute("ondragover", "allowDrop(event)");
+        container.append(box);
+
+        for(j = 1; j < 5; j++){
+            var list = [quiz[i][1], quiz[i][2], quiz[i][3], quiz[i][4]];
+
+            list = list.sort();
+
+            risposta = document.createElement("div");
+            risposta.setAttribute("class", "risposta");
+            risposta.setAttribute("id", "r" + i + j);
+            risposta.setAttribute("style", "width: 200px; text-align: center;");
+            risposta.setAttribute("draggable", "true");
+            risposta.setAttribute("ondragstart", "drag(event)");
+            risposta.append("-" + list[j-1]);
+            container.append(risposta);
+        }
+        $("body").append(container);
+        $("body").append("<br>");
+    }
+
+   
+
+    submit = document.createElement("button");
+    submit.setAttribute("onclick", "termina()");
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("style", "text-align: center;");
+    submit.append("Termina il quiz");
+    $("body").append(submit);
+
+}
+
+function timer() {
+    if(scaduto==false){
+        temp--;
+        document.getElementById("tempo").innerHTML = temp;
+    }
+}
+
+function temposcaduto() {
+    document.getElementById("tempo").innerHTML = 0;
+    scaduto=true;
+    alert("Tempo scaduto!");
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+
+    if(data == ("r02")){
+        corrette++;
+    }
+    if(data == ("r11")){
+        corrette++;
+    }
+    if(data == ("r23")){
+        corrette++;
+    }
+    if(data == ("r31")){
+        corrette++;
+    }
+    if(data == ("r43")){
+        corrette++;
+    }
+    if(data == ("r53")){
+        corrette++;
+    }
+    if(data == ("r61")){
+        corrette++;
+    }
+    if(data == ("r74")){
+        corrette++;
+    }
+    if(data == ("r81")){
+        corrette++;
+    }
+    if(data == ("r92")){
+        corrette++;
+    }
+    if(data == ("r102")){
+        corrette++;
+    }
+    if(data == ("r111")){
+        corrette++;
+    }
+    if(data == ("r123")){
+        corrette++;
+    }
+    if(data == ("r132")){
+        corrette++;
+    }
+    if(data == ("r141")){
+        corrette++;
+    }
+    if(data == ("r153")){
+        corrette++;
+    }
+    if(data == ("r163")){
+        corrette++;
+    }
+    if(data == ("r172")){
+        corrette++;
+    }
+    if(data == ("r184")){
+        corrette++;
+    }
+    if(data == ("r193")){
+        corrette++;
+    }
+    if(data == ("r202")){
+        corrette++;
+    }
+    if(data == ("r211")){
+        corrette++;
+    }
+    if(data == ("r224")){
+        corrette++;
+    }
+    if(data == ("r234")){
+        corrette++;
+    }
+    if(data == ("r241")){
+        corrette++;
+    }
+    if(data == ("r251")){
+        corrette++;
+    }
+    if(data == ("r264")){
+        corrette++;
+    }
+    if(data == ("r271")){
+        corrette++;
+    }
+    if(data == ("r283")){
+        corrette++;
+    }
+    if(data == ("r293")){
+        corrette++;
+    }
+    if(data == ("r304")){
+        corrette++;
+    }
+    if(data == ("r311")){
+        corrette++;
+    }
+    if(data == ("r324")){
+        corrette++;
+    }
+    if(data == ("r333")){
+        corrette++;
+    }
+
+}
+
+function termina() {
+    if(comp==false){
+        if(scaduto==false){
+            scaduto = true;
+            comp=true;
+            alert("Hai risposto correttamente a " + corrette + "/34 domande in " + (500-temp) + " secondi");
+        }
+        else{
+            comp=true;
+            alert("Non hai completato in tempo il quiz, ricarica la pagina e riprova\nHai risposto correttamente a " + corrette + "/34");
+        }
+    }else{
+        scaduto = true;
+        alert("Hai già completato il quiz! Se vuoi riprovare ricarica la pagina");
+    }
+}
